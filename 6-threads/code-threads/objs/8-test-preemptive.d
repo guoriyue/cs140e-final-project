755729c16cc218606836550bcc9c0c39e0ef772c
Guo Mingfei <1800012773@pku.edu.cn>
2024-03-09 18:49:47 -0800

    preemptive

---
+++ 6-threads/code-threads/objs/8-test-preemptive.d
@@ -0,0 +1,21 @@
+objs/8-test-preemptive.o: 8-test-preemptive.c preemptive-test-handler.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h \
+ /Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h \
+ preemptive-thread.h
+
+preemptive-test-handler.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/libc/demand.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/gpio.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/rpi-constants.h:
+
+/Users/guomingfei/Desktop/cs140e/cs140e-24win/libpi/include/your-prototypes.h:
+
+preemptive-thread.h:
