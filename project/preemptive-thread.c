89f624f0fc1b9c1bea605b2e50f75d3499cb6de6
Guo Mingfei <1800012773@pku.edu.cn>
2024-03-19 12:18:42 -0700

    fixed

--- project/preemptive-thread.c
+++ project/preemptive-thread.c
@@ -28,28 +28,6 @@ static unsigned nalloced = 0;
     printk("TRACE:%s:", __FUNCTION__); printk(args);    \
 } while(0)
 
-// static __attribute__((noreturn)) 
-// void schedule(void) 
-// {
-//     assert(cur_thread);
-
-//     pre_th_t *th = eq_pop(&runq);
-//     if(th) {
-//         output("switching from tid=%d,pc=%x to tid=%d,pc=%x,sp=%x\n", 
-//                 cur_thread->tid, 
-//                 cur_thread->regs.regs[REGS_PC],
-//                 th->tid,
-//                 th->regs.regs[REGS_PC],
-//                 th->regs.regs[REGS_SP]);
-//         eq_append(&runq, cur_thread);
-//         cur_thread = th;
-//     }
-//     uart_flush_tx();
-//     // mismatch_run(&cur_thread->regs);
-//     while (!uart_can_put8())
-//         ;
-//     switchto(&cur_thread->regs);
-// }
 
 static void interrupt_handler(regs_t *r) {
 
@@ -87,13 +65,9 @@ static inline regs_t regs_init(pre_th_t *p) {
 
     // XXX: which code had the partial save?  the ss rwset?
     regs_t regs = (regs_t) {
-        // .regs[4] = p->arg,
-        // .regs[5] = p->fn,
         .regs[0] = p->arg,
-        // .regs[REGS_PC] = (uint32_t)pre_init_trampoline,      // where we want to jump to
         .regs[REGS_PC] = p->fn,      // the stack pointer to use.
         .regs[REGS_SP] = p->stack_end,      // the stack pointer to use.
-        // .regs[REGS_LR] = (uint32_t)pre_exit, // where to jump if return.
         .regs[REGS_LR] = (uint32_t)sys_equiv_exit,
         .regs[REGS_CPSR] = cpsr & (~0b10000000),            // the cpsr to use.
     };
@@ -161,117 +135,13 @@ void scheduler(void) {
         // switchto(&cur_thread->regs);
         switchto_cswitch(&scheduler_thread->regs, &cur_thread->regs);
         trace("switching back to scheduler\n");
-        // trace("switching back to scheduler\n");
-        // if (first) {
-        //     switchto_cswitch(&scheduler_thread->regs, &cur_thread->regs);
-        //     first = 0;
-        //     // break;
-        // } else {
-        //     switchto(&cur_thread->regs);
-        // }
         
     }
 }
 
 enum {
     EQUIV_EXIT = 0,
-    EQUIV_PUTC = 1
 };
-
-
-// int syscall_vector(unsigned pc, regs_t *r) {
-//     // uint32_t inst, sys_num, mode;
-//     uint32_t mode;
-
-//     // inst = *(uint32_t *)pc;
-//     // sys_num = inst - 0xef000000;
-//     mode = spsr_get() & 0b11111;
-
-//     // put spsr mode in <mode>
-//     trace("mode=%b\n", mode);
-//     // IRQ_MODE
-//     // if(mode != USER_MODE && mode != SYS_MODE)
-//     //     panic("mode = %b: expected %b\n", mode, USER_MODE);
-//     // else
-//     //     trace("success: spsr is at user/sys level\n");
-
-//     cur_thread->regs = *r;
-//     eq_append(&runq, cur_thread);
-    
-//     cur_thread = scheduler_thread;
-//     switchto(&scheduler_thread->regs);
-//     // cswitch_from_exception(scheduler_thread->regs);
-//     return 0;
-//     // switch(sys_num) {
-//     // case REQUEST_STDOUT:
-//     //     th_trace("syscall: requesting stdout\n");
-//     //     if (stdio_is_idle(cur_thread->tid)) {
-//     //         write_stdio_tid(cur_thread->tid);
-//     //         cur_thread->block_reason = STDIO;
-//     //         cswitch_from_exception((uint32_t *)sp);
-//     //     } else {
-//     //         cur_thread->saved_sp = (uint32_t *)sp;
-//     //         cur_thread->block_reason = STDIO;
-//     //         Q_append(&blockq, cur_thread);
-//     //         cur_thread = scheduler_thread;
-//     //         cswitch_from_exception(scheduler_thread->saved_sp);
-//     //     }
-//     // case REQUEST_STDIN:
-//     //     th_trace("syscall: requesting stdin\n");
-//     //     if (stdio_is_idle(cur_thread->tid)) {
-//     //         // flush and enable rx interrupt?
-//     //         uart_flush_rx();
-//     //         uart_enable_interrupt(0);
-//     //         write_stdio_tid(cur_thread->tid);
-//     //         cur_thread->block_reason = STDIO;
-//     //         // don't return to thread -- wake it up until there is input
-//     //         cur_thread->saved_sp = (uint32_t *)sp;
-//     //         wait_stdin_thread = cur_thread;
-//     //         cur_thread = scheduler_thread;
-//     //         cswitch_from_exception(scheduler_thread->saved_sp);
-//     //     } else {
-//     //         cur_thread->saved_sp = (uint32_t *)sp;
-//     //         cur_thread->block_reason = STDIO;
-//     //         Q_append(&blockq, cur_thread);
-//     //         cur_thread = scheduler_thread;
-//     //         cswitch_from_exception(scheduler_thread->saved_sp);
-//     //     }
-//     // case WAIT_STDOUT: 
-//     //     th_trace("syscall: waiting stdout\n");
-//     //     uart_enable_interrupt(1);
-//     //     if (stdout_queue_is_empty()) {
-//     //         write_stdio_tid(0);
-//     //         cur_thread->block_reason = NONE;
-//     //         cswitch_from_exception((uint32_t *)sp);
-//     //     } else {
-//     //         // create a non-interruptable printer thread
-//     //         pre_thread_t *th_printer = pre_fork(&stdout_queue_to_txfifo, NULL, 0, 1);
-//     //         th_printer->block_reason = STDIO;
-//     //         write_stdio_tid(th_printer->tid);
-//     //         cur_thread->saved_sp = (uint32_t *)sp;
-//     //         wait_stdout_thread = cur_thread;
-//     //         cur_thread = scheduler_thread;
-//     //         cswitch_from_exception(scheduler_thread->saved_sp);
-//     //     }
-//     // case END_STDIN:
-//     //     th_trace("syscall: ended stdout\n");
-//     //     // DRAIN stdin queue?
-//     //     // disable uart rx interrupt
-//     //     uart_disable_interrupt(0);
-//     //     write_stdio_tid(0);
-//     //     wait_stdout_thread->block_reason = NONE;
-//     //     cswitch_from_exception((uint32_t *)sp);
-//     // case NRF_SPI_TRANSFER:
-//     //     spi_transfer_syscall_handler(pc, sp, arg1, (uint32_t)arg2, arg3, (unsigned)arg4);
-//     //     cswitch_from_exception((uint32_t *)sp);
-//     // default: 
-//     //     if (uart_interrupt_is_on()) {
-//     //         uart_reset_int_off();
-//     //     }
-//     //     trace("Illegal system call = %d!\n", sys_num);
-//     //     clean_reboot();
-//     // }
-// }
 
 static int equiv_syscall_handler(regs_t *r) {
     trace("syscall handler is called.\n");
@@ -427,5 +297,5 @@ void pre_init(void) {
 
 void pre_exit(void) {
     printk("thread=%d exited\n", cur_thread->tid);
-    // switchto(&scheduler_thread->regs);
+    switchto(&scheduler_thread->regs);
 }
